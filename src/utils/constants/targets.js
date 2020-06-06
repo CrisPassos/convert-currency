9b250ebb85abd21ed410d811ad463685f52aa8a4
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-06 19:19:27 +0100

    add tagets and currencies

---
+++ src/utils/constants/targets.js
@@ -0,0 +1,10 @@
+export const TARGETS = [
+  { name: "EUR", image: "../../assets/images/EUR.png" },
+  { name: "BAT", image: "../../assets/images/BAT.png" },
+  { name: "BTC", image: "../../assets/images/BTC.png" },
+  { name: "BCH", image: "../../assets/images/BCH.png" },
+  { name: "CNY", image: "../../assets/images/CNY.png" },
+  { name: "ETH", image: "../../assets/images/ETH.png" },
+  { name: "GBP", image: "../../assets/images/GBP.png" },
+  { name: "BRL", image: "../../assets/images/BRL.png" },
+];
