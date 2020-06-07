6f29e8f4c45b690b5eef94401fd869b201d6f327
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-07 10:58:08 +0100

    add input and results

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,29 +1,73 @@
 import React, { useEffect, useState } from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Select from "../../components/Select";
+import { TARGETS } from "../../utils/constants/targets";
 
 function HomePage() {
-  const [currency, setCurrency] = useState([]);
+  const [currencies, setCurrencies] = useState([]);
 
-  useEffect(
-    () => {
-      setCurrency("testes");
-      console.log(currency);
-    },
-    [currency]
-  );
+  useEffect(() => {
+    setCurrencies([
+      "0.11",
+      "0.22",
+      "0.33",
+      "0.44",
+      "0.55",
+      "0.66",
+      "0.77",
+      "0.88",
+    ]);
+  }, []);
 
   function getSelection(e) {
     console.log(e);
   }
 
+  function getValue(e) {
+    console.log(e.target.value);
+  }
+
+  const targetValue = TARGETS.map(item => {
+    return (
+      <li key={item.name}>
+        <img src={item.image} alt={item.name} />
+        <span>{item.name}</span>
+      </li>
+    );
+  });
+
+  const convertValue = currencies.map(item => {
+    return <li key={item}>{item}</li>;
+  });
+
   return (
     <div>
       <Header />
-      <div>
-        <Select onChange={getSelection} />
-      </div>
+      <Container>
+        <Row md={3}>
+          <Col>
+            <Form.Control type="number" onChange={e => getValue(e)} />
+            {/* <CurrencyFormat thousandSeparator={true} onValueChange={(e) => console.log(e)}/> */}
+          </Col>
+          <Col>
+            <Select onChange={getSelection} />
+          </Col>
+          <Col />
+        </Row>
+        <Row>
+          <Col>
+            <ul>{convertValue}</ul>
+          </Col>
+          <Col>
+            <ul>{targetValue}</ul>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
--- src/utils/constants/targets.js
+++ src/utils/constants/targets.js
@@ -1,10 +1,10 @@
 export const TARGETS = [
-  { name: "EUR", image: "../../assets/images/EUR.png" },
-  { name: "BAT", image: "../../assets/images/BAT.png" },
-  { name: "BTC", image: "../../assets/images/BTC.png" },
-  { name: "BCH", image: "../../assets/images/BCH.png" },
-  { name: "CNY", image: "../../assets/images/CNY.png" },
-  { name: "ETH", image: "../../assets/images/ETH.png" },
-  { name: "GBP", image: "../../assets/images/GBP.png" },
-  { name: "BRL", image: "../../assets/images/BRL.png" },
+  { name: "EUR", image: require("../../assets/images/EUR.png") },
+  { name: "BAT", image: require("../../assets/images/BAT.png") },
+  { name: "BTC", image: require("../../assets/images/BTC.png") },
+  { name: "BCH", image: require("../../assets/images/BCH.png") },
+  { name: "CNY", image: require("../../assets/images/CNY.png") },
+  { name: "ETH", image: require("../../assets/images/ETH.png") },
+  { name: "GBP", image: require("../../assets/images/GBP.png") },
+  { name: "BRL", image: require("../../assets/images/BRL.png") },
 ];
