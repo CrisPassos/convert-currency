8632dcf6dca4d42c37019340e833647368d5d39d
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-08 11:48:21 +0100

    add format select

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import OPTION from "react-select";
 import { CURRENCIES } from "../../utils/constants/currencies";
 
+import "./styles.scss";
+
 const Select = ({onChange}) => {
 
   onChange = onChange ? onChange : () => { };
 
   return (
     <>
-      <OPTION options={CURRENCIES} defaultValue={CURRENCIES[29]} onChange={e => onChange(e)} />
+      <OPTION options={CURRENCIES} defaultValue={CURRENCIES[29]} onChange={e => onChange(e)} className="format-select float-right" />
     </>
   );
 };
